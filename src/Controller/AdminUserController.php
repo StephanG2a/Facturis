c518c0fa08c5b0350ce97c6a0d40e81238d69c4a
Stephan2a <stephan.gueorguieff@gmail.com>
2024-03-02 04:28:39 +0100

    In progress edit profile

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -7,14 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/dashboard/profile', name: 'dashboard_profile_')]
-#[IsGranted("ROLE_USER")]
-class UserController extends AbstractController
+#[Route('/dashboard/users', name: 'dashboard_users_')]
+#[IsGranted("ROLE_ADMIN")]
+class AdminUserController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('adminUser/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
