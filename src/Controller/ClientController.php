03a2ac7ebf0829769f3be6f8092634ed5c109cfe
Stephan2a <stephan.gueorguieff@gmail.com>
2024-03-03 17:46:11 +0100

    CRUD Service & Category

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/dashboard', name: 'dashboard_client_')]
+#[Route('/dashboard/client', name: 'dashboard_client_')]
 #[IsGranted("ROLE_USER")]
 class ClientController extends AbstractController
 {
