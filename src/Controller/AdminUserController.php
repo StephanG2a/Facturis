99f7cf2b6331bb800cb0a5797c725c9bd0e647ce
Stephan2a <stephan.gueorguieff@gmail.com>
2024-03-03 10:54:33 +0100

    Styling FlashMessages

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -2,20 +2,67 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use App\Form\UserType;
 
 #[Route('/dashboard/users', name: 'dashboard_users_')]
 #[IsGranted("ROLE_ADMIN")]
 class AdminUserController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $users = $userRepository->findAll();
+
+        $filteredUsers = array_filter($users, function ($user) {
+            return !in_array('ROLE_ADMIN', $user->getRoles());
+        });
+
+        $pagination = $paginator->paginate(
+            $filteredUsers,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('adminUser/index.html.twig', [
-            'controller_name' => 'UserController',
+            'pagination' => $pagination,
         ]);
+    }
+
+    #[Route('/{id}', name: 'edit')]
+    public function edit(Request $request, User $user, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->remove('password');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'User updated successfully.');
+            return $this->redirectToRoute('dashboard_users_index');
+        }
+
+        return $this->render('adminUser/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(User $user, EntityManagerInterface $em): Response
+    {
+        $em->remove($user);
+        $em->flush();
+        $this->addFlash('success', 'User deleted successfully.');
+        return $this->redirectToRoute('dashboard_users_index');
     }
 }
